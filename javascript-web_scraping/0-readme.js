e7666e2725a1b656e452d5f556f4af33f2b9d02e
Stefano-BML <stefanoml1128@gmail.com>
2023-09-24 06:50:49 +0000

    fix 0 readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,11 +3,16 @@
 
 const fs = require('fs');
 
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function (err, data) {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     console.log(data);
   }
