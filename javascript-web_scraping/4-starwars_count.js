21de24f7b1471dc4ce13913d675f05183fd88fcf
Stefano-BML <stefanoml1128@gmail.com>
2023-09-25 00:49:04 +0000

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 /* task 4 starwars count js */
-const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  }
-  let count = 0;
-  for (const result of JSON.parse(body).results) {
-    for (const wedgeUrl of result.characters) {
-      if (wedgeUrl.includes(18)) {
-        count++;
+const request = require('request');
+let nFilms = 0;
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
+        }
       }
     }
   }
-  console.log(count);
+  console.log(nFilms);
 });
