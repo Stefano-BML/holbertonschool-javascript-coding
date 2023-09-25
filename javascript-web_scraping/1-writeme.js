1fa4fb6aec95a3573d73ce17e17aa5eba80dd5fe
Stefano-BML <stefanoml1128@gmail.com>
2023-09-25 00:33:50 +0000

    task 1 writeme

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,22 +2,4 @@
 /* task 1 writeme js */
 
 const fs = require('fs');
-
-// Check if the user provided the correct number of arguments
-if (process.argv.length !== 4) {
-  console.error('Usage: node 1-writeme.js <file-path> <string-to-write>');
-  process.exit(1);
-}
-
-// Get the file path and string to write from the command line arguments
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Write the string to the file in utf-8 encoding
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`"${stringToWrite}" has been written to ${filePath}`);
-  }
-});
+fs.writeFileSync(process.argv[2], process.argv[3]);
