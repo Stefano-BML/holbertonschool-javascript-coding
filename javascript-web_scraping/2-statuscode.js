da95b88e93974902961d825a0311db0666017198
Stefano-BML <stefanoml1128@gmail.com>
2023-09-25 00:39:23 +0000

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* task 2 statuscode js */
+
 const request = require('request');
-
-const url = process.argv[2];
-
-request.get(url).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
 });
