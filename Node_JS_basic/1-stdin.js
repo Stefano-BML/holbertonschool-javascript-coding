d936f7a82a048355bd6eb0e43b94c940e9bde278
Stefano-BML <stefanoml1128@gmail.com>
2023-10-11 23:40:01 +0000

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  rl.close();
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});
