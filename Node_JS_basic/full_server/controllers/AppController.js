b55ced16fecd6207ff9fb079c2d68977454eda13
Stefano-BML <stefanoml1128@gmail.com>
2023-10-12 00:45:57 +0000

    control

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,9 +1,7 @@
-// full_server/controllers/AppController.js
-
 class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.status(200);
+    response.send('Hello Holberton School!');
   }
 }
-
-export default AppController;
+module.exports = AppController;
